e25668e9c35c715126afe5cac3401e0a9e0d8303
Ramazan DOĞAN <doganrmzn40@gmail.com>
2024-07-30 12:06:57 +0300

    Lint

--- src/components/blurhash.tsx
+++ src/components/blurhash.tsx
@@ -1,5 +1,5 @@
 import { Blurhash } from "react-blurhash";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 export default function BlurHash({
   src,
